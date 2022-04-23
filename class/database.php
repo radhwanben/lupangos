a565bcc90b04cf1588d70dc4cf83e46348d46569
radhwanben <tirexman7@gmail.com>
2022-04-23 08:44:14 +0100

    add database login

--- class/database.php
+++ class/database.php
@@ -5,7 +5,7 @@ class database{
     static function connection()
 	{
 		try {
-			$conn = new \PDO("mysql:host=localhost;dbname=lupangos", 'root', '');
+			$conn = new \PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_d52e262c7fd0519", 'b33fc0383cf56d', 'b45185ce');
 	        $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		} catch (\Exception $e) {
 			 $conn = null;
