8087f2efa23b73b4e70d0ed69efecae755f9bca7
radhwanben <tirexman7@gmail.com>
2022-04-23 08:53:17 +0100

    add database loginnn

---
+++ config/sql.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('class/Database.php');
+
+  try {
+
+    $db = Database::connection();
+
+    // sql to create table
+    $users = "CREATE TABLE IF NOT EXISTS users (
+  `user_id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   `user_name` VARCHAR( 255 ) NOT NULL ,
+   `user_email` VARCHAR( 60 ) NOT NULL ,
+   `user_pass` VARCHAR( 255 ) NOT NULL ,
+    UNIQUE (`user_name`),
+    UNIQUE (`user_email`)
+) ENGINE = MYISAM ;
+";
+try{
+    $db->exec($users);
+    echo "Tables created successfully";
+}catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+
+  $db = null;
