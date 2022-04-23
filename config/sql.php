06e9d3b9ba89c799038bd8dae060779918aaf218
radhwanben <tirexman7@gmail.com>
2022-04-23 09:04:42 +0100

    add database logi

--- config/sql.php
+++ config/sql.php
@@ -7,14 +7,18 @@ require_once('../class/database.php');
     $db = database::connection();
 
     // sql to create table
-    $users = "CREATE TABLE IF NOT EXISTS users (
-  'user_id' INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+
+$users = "CREATE TABLE IF NOT EXISTS users (
+    'user_id' INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
    'user_name' VARCHAR( 255 ) NOT NULL ,
   'user_email' VARCHAR( 60 ) NOT NULL ,
    'user_pass' VARCHAR( 255 ) NOT NULL ,
     UNIQUE ('user_name'),
     UNIQUE ('user_email')
-);";
+    )";
+
+
+
 try{
     $db->exec($users);
     echo "Tables created successfully";
